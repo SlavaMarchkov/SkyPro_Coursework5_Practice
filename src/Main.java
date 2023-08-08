a5edff9556c096ac9366f7737d95ee4bad692b63
SlavaMarchkov <slavamarchkov@yandex.ru>
2023-08-08 15:04:05 +0700

    Решение задания

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int number1 = 40;
+        int number2 = 0;
+        try {
+            if (number2 == 0) {
+                throw new MyOwnException("Делить на ноль нельзя!");
+            }
+            int result = number1 / number2;
+            System.out.println("Result: " + result);
+            // some code
+        } catch (MyOwnException e) {
+            System.out.println("Error: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Other error: " + e.getMessage());
+        }
+        finally {
+            System.out.println("Done!");
+        }
+    }
+}
+
+class MyOwnException extends RuntimeException {
+    public MyOwnException(String message) {
+        super(message);
     }
 }
